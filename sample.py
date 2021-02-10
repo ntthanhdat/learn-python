f945310f857f8a8fddede45b463dec3c3f14f7c0
ntthanhdat <thanhdatnguyenthe@gmail.com>
2021-02-10 22:30:09 +0700

    create base html

--- sample.py
+++ sample.py
@@ -1,5 +1,6 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for, render_template, request
 from markupsafe import escape
+from jinja2 import Template
 
 app = Flask("web1")
 
@@ -7,10 +8,12 @@ app = Flask("web1")
 def index():
     return 'index'
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return 'login'
-
+    if request.method == 'POST':
+        return do_the_login()
+    else:
+        return show_the_login_form()
 
 @app.route('/hello/')
 @app.route('/hello/<name>')
